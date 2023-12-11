e33a42493151e0370c008981a2b95c13ab4a00fc
SONY DADU <78530922+Sony1727@users.noreply.github.com>
2023-12-11 08:40:57 +0530

    Create THIS.java

---
+++ THIS.java
@@ -0,0 +1,26 @@
+public class Hello
+
+{
+
+    public static  void main(String args[]) {
+
+        Sample s = new Sample(190, 120); {
+
+
+        }
+    }
+    }
+
+
+public class Sample {
+    int j=40 ,k=78;
+    Sample(int a,int b){
+        this.j=a;
+        this.k=b;
+      System.out.println("a :"+a +"b :"+b);
+    }
+}
+
+
+
+
