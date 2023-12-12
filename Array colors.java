202af32af32b9ecf81cc0a98b23b0addb93b2b8f
SONY DADU <78530922+Sony1727@users.noreply.github.com>
2023-12-12 18:54:13 +0530

    Create Array colors.java

---
+++ Array colors.java
@@ -0,0 +1,14 @@
+public class Main
+{
+
+    public static void main(String args[])
+    {
+        String[] n=new String[3];
+        String[] colors ={"Red","Green","Blue"};
+        System.out.println(colors[2]);
+        colors[2] ="peech";
+        System.out.println(colors[2]);
+    }
+
+}
+
